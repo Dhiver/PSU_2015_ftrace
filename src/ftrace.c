f0bb5e54f7034bb20832ceb9f709ba25b4f27249
florian videau <florian.videau@epitech.eu>
2016-04-12 15:58:13 +0200

    fork

--- src/ftrace.c
+++ src/ftrace.c
@@ -5,18 +5,28 @@
 ** Login   <videau_f@epitech.net>
 **
 ** Started on  Tue Apr 12 11:59:11 2016 florian videau
-** Last update Tue Apr 12 15:10:11 2016 florian videau
+** Last update Tue Apr 12 15:58:01 2016 florian videau
 */
 
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "ftrace.h"
 
 int	run_process(char *file)
 {
   char	*file_w_path;
+  int	pid;
 
   if ((file_w_path = find_executable(file)) == NULL)
     return (fprintf(stderr, "Can't find or execute '%s'\n", file), 1);
+  if ((pid = fork()) == 1)
+    return (fprintf(stderr, "%s\n", strerror(errno)), -1);
+  if (!pid)
+    /* return (be_the_child(file_w_path)); */{}
+  else
+    /* return (be_the_parent(file_w_path)); */{}
   return 0;
 }
 
